4110181d14faa407f7716f807dc320d68bcf6e56
prasanthbangs2016 <prasanthbangs@gmail.com>
2018-06-07 23:33:15 +0530

    day7 - day8

---
+++ 10-simple-if.py
@@ -0,0 +1,4 @@
+var01 =25  # constant value with variable
+if var01:
+ print ("Defined variable has a value and printing the same")
+ print ("the values is:",var01)
---
+++ 12-simple-if-else.py
@@ -0,0 +1,7 @@
+var01=input("enter any interger value:")
+if var01:
+   print ("variable has value working in true condition")
+   print ("True condition value is:",var01)
+else:
+  print ("varible doesnt have a value working in false condition")
+  print ("The false condition value is:",var01)
